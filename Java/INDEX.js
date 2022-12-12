863aa6abc6fc50d7dbfc57f1934a3e3bed3333e8
erikkjell-1 <erkan26@live.se>
2022-12-12 12:00:50 +0100

    PushilyPusch

--- Java/INDEX.js
+++ Java/INDEX.js
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +17,54 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+const titleElem = document.querySelector('#movie');
+const yearElem = document.querySelector('#release');
+const ratingElem = document.querySelector('#score');
+const categoryElem = document.querySelector('#categories');
+const sendBtn = document.querySelector('#send');
+const elemResult = document.querySelector('#database');
+
+sendBtn.addEventListener('click', () => {
+    const title = titleElem.value;
+    const year = yearElem.value;
+    const rating = ratingElem.value;
+    const genre = categoryElem.value;
+
+    saveToFirebase(title, year, genre, rating);
+});
+
+async function saveToFirebase(title, year, genre, rating) {
+    try {
+        await addDoc(collection(db, 'Movies'), { 
+            Title: title,
+            Released: year,
+            Genre: genre,
+            Rating: rating,
+            
+        });
+    } catch (error) {
+        console.log('ERROR:', error);
+    }
+}
+
+async function getAllMovies(){
+    const movies = await getDocs(collection(db, 'Movies'));
+
+    movies.forEach((movie) => {
+        console.log(movie)
+        const elem = `         <card><p>
+                               ${movie.data().Title}<br>
+                               Released: ${movie.data().Released}<br>
+                               Genre: ${movie.data().Genre}<br>
+                               Rating: ${movie.data().Rating}
+                               </p></card>`
+        elemResult.insertAdjacentHTML('beforeend', elem)
+    })
+}
+
+getAllMovies();
+
+    
 
 
 
